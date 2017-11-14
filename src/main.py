cc4dfa637116eaa62e8f0fa61212c1937c4a2fb4
shivasiddharth <ushivasiddharth@gmail.com>
2017-11-14 15:35:03 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,123 +16,23 @@
 
 
 from __future__ import print_function
+import sys
+import threading
 import RPi.GPIO as GPIO
 import argparse
 import os.path
 import os
 import json
 import subprocess
+import RPi.GPIO as GPIO
 import google.oauth2.credentials
 from google.assistant.library import Assistant
 from google.assistant.library.event import EventType
 from google.assistant.library.file_helpers import existing_file
-from actions import Action
-from actions import YouTube
-from actions import stop
-from actions import radio
-from actions import ESP
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-#Indicator Pins
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
-GPIO.output(5, GPIO.LOW)
-GPIO.output(6, GPIO.LOW)
-led=GPIO.PWM(25,1)
-led.start(0)
-
-
-
-
-def process_event(event):
-    """Pretty prints events.
-
-    Prints all events that occur with two spaces between each new
-    conversation and a single space between turns of a conversation.
-
-    Args:
-        event(event.Event): The current event to process.
-    """
-    if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-        subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Fb.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        GPIO.output(5,GPIO.HIGH)
-        led.ChangeDutyCycle(100)
-
-    if (event.type == EventType.ON_RESPONDING_STARTED and event.args and not event.args['is_error_response']):
-       GPIO.output(5,GPIO.LOW)
-       GPIO.output(6,GPIO.HIGH)
-       led.ChangeDutyCycle(50)
-
-    if event.type == EventType.ON_RESPONDING_FINISHED:
-       GPIO.output(6,GPIO.LOW)
-       GPIO.output(5,GPIO.HIGH)
-       led.ChangeDutyCycle(100)
-
-
-    print(event)
-
-    if (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and
-            event.args and not event.args['with_follow_on_turn']):
-        GPIO.output(5,GPIO.LOW)
-        led.ChangeDutyCycle(0)
-        print()
-
-
-def main():
-
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--credentials', type=existing_file,
-                        metavar='OAUTH2_CREDENTIALS_FILE',
-                        default=os.path.join(
-                            os.path.expanduser('/home/pi/.config'),
-                            'google-oauthlib-tool',
-                            'credentials.json'
-                        ),
-                        help='Path to store and read OAuth2 credentials')
-    args = parser.parse_args()
-    with open(args.credentials, 'r') as f:
-        credentials = google.oauth2.credentials.Credentials(token=None,
-                                                            **json.load(f))
-
-    with Assistant(credentials) as assistant:
-        subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Startup.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        for event in assistant.start():
-            process_event(event)
-            usrcmd=event.args
-            if 'trigger'.lower() in str(usrcmd).lower():
-                assistant.stop_conversation()
-                Action(str(usrcmd).lower())
-            if 'play'.lower() in str(usrcmd).lower():
-                assistant.stop_conversation()
-                YouTube(str(usrcmd).lower())
-            if 'stop'.lower() in str(usrcmd).lower():
-                stop()
-            if 'tune into'.lower() in str(usrcmd).lower():
-                assistant.stop_conversation()
-                radio(str(usrcmd).lower())
-            if 'wireless'.lower() in str(usrcmd).lower():
-                assistant.stop_conversation()
-                ESP(str(usrcmd).lower())
-
-if __name__ == '__main__':
-    main()
-import logging
-import sys
-import threading
-
-import aiy.assistant.auth_helpers
-import aiy.voicehat
-from google.assistant.library import Assistant
-from google.assistant.library.event import EventType
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 
 class MyAssistant(object):
@@ -154,32 +54,44 @@ class MyAssistant(object):
         self._task.start()
 
     def _run_task(self):
-        credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
+        parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+        parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('/home/pi/.config'),
+                            'google-oauthlib-tool',
+                            'credentials.json'
+                        ),
+                        help='Path to store and read OAuth2 credentials')
+        args = parser.parse_args()
+        with open(args.credentials, 'r') as f:
+            credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
         with Assistant(credentials) as assistant:
             self._assistant = assistant
             for event in assistant.start():
                 self._process_event(event)
 
     def _process_event(self, event):
-        status_ui = aiy.voicehat.get_status_ui()
+       
         if event.type == EventType.ON_START_FINISHED:
-            status_ui.status('ready')
             self._can_start_conversation = True
             # Start the voicehat button trigger.
-            aiy.voicehat.get_button().on_press(self._on_button_pressed)
+            if not GPIO.input(24):
+                (self._on_button_pressed)
             if sys.stdout.isatty():
                 print('Say "OK, Google" or press the button, then speak. '
                       'Press Ctrl+C to quit...')
 
         elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
             self._can_start_conversation = False
-            status_ui.status('listening')
+            
 
         elif event.type == EventType.ON_END_OF_UTTERANCE:
-            status_ui.status('thinking')
+            print()
 
         elif event.type == EventType.ON_CONVERSATION_TURN_FINISHED:
-            status_ui.status('ready')
             self._can_start_conversation = True
 
         elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
@@ -192,7 +104,6 @@ class MyAssistant(object):
         # 2. The assistant library is already in a conversation.
         if self._can_start_conversation:
             self._assistant.start_conversation()
-
 
 def main():
     MyAssistant().start()
